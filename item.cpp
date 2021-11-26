9f346fd039eb05368b01c070d2621f9d07086ad0
kwo20 <89323338+kwo20@users.noreply.github.com>
2021-11-26 09:38:50 -0600

    Added item class

---
+++ item.cpp
@@ -0,0 +1,46 @@
+#include "item.h"
+
+void item::setName(string item_name)
+{
+  this->item_name = item_name;
+}
+
+void item::setQuantity(int quantity)
+{
+  this->quantity = quantity;
+}
+
+void item::setPrice(float price)
+{
+  this->price = price;
+}
+
+string item::getName()
+{
+  return item_name;
+}
+
+int item::getQuantity()
+{
+  return quantity;
+}
+
+float item::getPrice()
+{
+  return price;
+}
+
+void item::decreaseQuantity(int quantity)
+{
+  this->quantity-=quantity;
+}
+
+void item::increaseQuantity(int quantity)
+{
+  this->quantity+=quantity;
+}
+
+void item::viewItem()
+{
+  cout << "Name: " << item_name << " " << "Quantity: " << quantity << " " << "Price: " << price << "\n";
+}
