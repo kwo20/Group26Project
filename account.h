5c8c6ce16b7c455ff7b55e92cc0ed394df668826
kwo20 <89323338+kwo20@users.noreply.github.com>
2021-11-26 18:30:29 -0600

    Update account.h
    Gives the user a cart and adds functions to work with the cart

--- account.h
+++ account.h
@@ -3,28 +3,37 @@
 #include <string>
 #include <iostream>
 
+#include "cart.h"
+#include "item.h"
+
 using namespace std;
-class account
+class account : public cart
 {
   public:
     string username;
     string password;
     string shipping_info;
     string payment_info;
+    cart account_cart;
 
     //setters
     void setUser(string username);
     void setPassword(string password);
     void setShipping(string shipping_info);
     void setPayment(string payment_info);
+    void setCartSize(int cart_size);
 
     //getters
     string getUser();
     string getPass();
     string getShipping();
     string getPayment();
+    void checkout();
 
     void printAccount();
+    void add_item(int quantity, class item item_to_add);
+    void printCart();
+    void remove_item(int item_number);
 
     //create an account with information
     account(string username, string password, string shipping_info, string payment_info) : username(username), password(password), shipping_info(shipping_info), payment_info(payment_info) {}
@@ -32,6 +41,4 @@ class account
     //create a blank account
     account() : username(""), password(""), shipping_info(""), payment_info("") {}
 
-    //destructor
-    //~account();
 };
