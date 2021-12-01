9404de60a42ca11d9764c59f102bff7d4e8f7335
jacobscoopy <95316473+jacobscoopy@users.noreply.github.com>
2021-12-01 11:10:19 -0600

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,50 @@
+#pragma once
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#include "cart.h"
+#include "item.h"
+
+using namespace std;
+class admin : public cart
+{
+public:
+    string username;
+    string password;
+    string shipping_info;
+    string payment_info;
+    string adminpwd;
+    cart account_cart;
+
+    //setters
+    void setUser(string username);
+    void setPassword(string password);
+    void setShipping(string shipping_info);
+    void setPayment(string payment_info);
+    void setAdminpwd(string adminpwd);
+    void setCartSize(int cart_size);
+
+    //getters
+    string getUser();
+    string getPass();
+    string getShipping();
+    string getPayment();
+    void checkout();
+    void viewHistory();
+    void clearCart();
+
+    void printAccount();
+    void add_item(int quantity, class item item_to_add);
+    void printCart();
+    void remove_item(int item_number);
+    void showinfo();
+
+    //create an account with information
+    account(string username, string password, string shipping_info, string payment_info) : username(username), password(password), shipping_info(shipping_info), payment_info(payment_info) {}
+
+    //create a blank account
+    account() : username(""), password(""), shipping_info(""), payment_info("") {}
+
+};
